23a1c82c657533c53a0e2ecf4e3d276bbea00ca2
jonnss <jonns.barbosa@gmail.com>
2020-12-10 19:51:46 -0600

    MVC

--- mvc/model/database.php
+++ mvc/model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=colegio;charset=utf8', 'root', 'jhonatan12');
+        $pdo = new PDO('mysql:host=localhost;dbname=colegio;charset=utf8', 'jhonatanspec', 'AMalexturnerAM12');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
